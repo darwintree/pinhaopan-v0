de86c5736ae3ac0da432e109c39816fd09ef0cb5
darwintree <17946284+darwintree@users.noreply.github.com>
2025-04-10 19:51:43 +0800

    feat: enhance webpack configuration to include splitChunks optimization for vendor modules

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,13 @@ const nextConfig = {
   },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false, crypto: false };
+    config.optimization.splitChunks.cacheGroups = {
+      commons: {
+        test: /[\\/]node_modules[\\/]/,
+        name: "vendors",
+        chunks: "all",
+      },
+    };
     return config;
   },
 };
